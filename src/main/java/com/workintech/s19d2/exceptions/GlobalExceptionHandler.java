82feff97079e61464dcc02132c17d62fe86b0678
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-07 23:41:29 +0400

    Create GlobalExceptionHandler.java

---
+++ src/main/java/com/workintech/s19d2/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.workintech.s19d2.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@ControllerAdvice
+
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    ResponseEntity<AccountErrorResponse> handleException (AccountException accountException) {
+        log.error("An Account error has occurred " + accountException);
+        AccountErrorResponse accountErrorResponse = new AccountErrorResponse(accountException.getHttpStatus().value(), accountException.getMessage(), LocalDateTime.now());
+        return new ResponseEntity<>(accountErrorResponse,accountException.getHttpStatus());
+    }
+
+    
+}
