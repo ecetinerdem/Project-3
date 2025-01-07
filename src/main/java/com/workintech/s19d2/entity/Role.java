be343728f2b2da777c44a8e5a9c0b56369a0df0d
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-07 20:08:50 +0400

    Update Role.java

--- src/main/java/com/workintech/s19d2/entity/Role.java
+++ src/main/java/com/workintech/s19d2/entity/Role.java
@@ -1,4 +1,57 @@
 package com.workintech.s19d2.entity;
 
-public class Role {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "role", schema = "fsweb")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Role implements UserDetails {
+    Long id;
+    String Authority;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
