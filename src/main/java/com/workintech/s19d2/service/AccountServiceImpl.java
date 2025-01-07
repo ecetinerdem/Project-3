ae469024f37644e13f59c76922683f8c562f6538
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-07 20:57:49 +0400

    Update AccountServiceImpl.java

--- src/main/java/com/workintech/s19d2/service/AccountServiceImpl.java
+++ src/main/java/com/workintech/s19d2/service/AccountServiceImpl.java
@@ -1,18 +1,35 @@
 package com.workintech.s19d2.service;
 
 import com.workintech.s19d2.entity.Account;
+import com.workintech.s19d2.repository.AccountRepository;
+import com.workintech.s19d2.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+
+@AllArgsConstructor
+@Service
+@Data
 public class AccountServiceImpl implements AccountService{
+
+    private final AccountRepository accountRepository;
+    private  final MemberRepository memberRepository;
+
+
     @Override
     public List<Account> findAll() {
-        return List.of();
+        return accountRepository.findAll();
     }
 
     @Override
     public Account findById(Long id) {
-        return null;
+        return accountRepository.findById(id).orElseThrow(() -> new AccountException("Account does not exist with id " + id, HttpStatus.NOT_FOUND));
     }
 
     @Override
