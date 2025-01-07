027430c56fe111afa8c4919cbd7a4af62fb24ef4
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-07 23:56:00 +0400

    add Member service

--- src/main/java/com/workintech/s19d2/service/MemberService.java
+++ src/main/java/com/workintech/s19d2/service/MemberService.java
@@ -1,4 +1,13 @@
 package com.workintech.s19d2.service;
 
+import com.workintech.s19d2.entity.Member;
+
+import java.util.List;
+
 public interface MemberService {
+    List<Member> findAll();
+    Member findById(Long id);
+    Member findByEmail(String email);
+    Member save(Member member);
+    Member delete(Long id);
 }
---
+++ src/main/java/com/workintech/s19d2/service/MemberServiceImpl.java
@@ -0,0 +1,45 @@
+package com.workintech.s19d2.service;
+
+import com.workintech.s19d2.entity.Member;
+import com.workintech.s19d2.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+@Data
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public List<Member> findAll() {
+        return memberRepository.findAll();
+    }
+
+    @Override
+    public Member findById(Long id) {
+        return memberRepository.findById(id).orElseThrow(() -> new MemberException("Member does not exist with id: " + id, HttpStatus.NOT_FOUND));
+    }
+
+    @Override
+    public Member findByEmail(String email) {
+        return memberRepository.findByEmail(email).orElseThrow(() -> new MemberException("Member does not exist with email: " + email, HttpStatus.NOT_FOUND));
+    }
+
+    @Override
+    public Member save(Member member) {
+        return memberRepository.save(member);
+    }
+
+    @Override
+    public Member delete(Long id) {
+        Member foundMember = findById(id);
+        memberRepository.delete(foundMember);
+        return foundMember;
+    }
+}
